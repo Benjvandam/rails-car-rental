6278adf41ab652e0d3a2c61780cda7d49512791f
Humberto Pedra <hphumbertopedra@yahoo.com.br>
2023-08-30 15:30:21 -0300

    created reviews in the car show page nevertheless

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :cars do
     resources :bookings, only: %i[index create]
-    resources :reviews, only: %i[index create]
+    resources :reviews, only: %i[new create]
   end
 
   resources :bookings, only: %i[index destroy edit update]
