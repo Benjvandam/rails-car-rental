f6a9f9a75d8a83a83126d60521408e306519bfd0
Pedro Soriano Ribeiro <psorianoribeir00@mylangara.ca>
2023-08-31 16:50:23 -0300

    Adding validations to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,22 @@
 class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
+  require 'date'
 
   validates :price, :date_from, :date_until, presence: true
   # Car must be available for booking to happen - TBV
-  # Cant rent for pas date - TBV
-  # validates :date_from, comparison: { greater_than: :date.now }
+  validates :date_from, comparison: { greater_than: Date.today }
   validates :date_until, comparison: { greater_than: :date_from }
+  validate :no_overlapping_bookings
+
+  private
+
+  def no_overlapping_bookings
+    if Booking.where(car_id: car_id)
+              .where.not(id: id) # Exclude the current booking when updating
+              .where("(date_from <= ? AND date_until >= ?) OR (date_from >= ? AND date_from <= ?)", date_until, date_from, date_from, date_until)
+              .exists?
+      errors.add(:base, "Another booking already exists for this car during this period")
+    end
+  end
 end
