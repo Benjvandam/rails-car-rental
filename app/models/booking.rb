b0bd18f7c1dc50cd4b9433641070faa03efa020e
Pedro Soriano Ribeiro <psorianoribeir00@mylangara.ca>
2023-08-31 12:29:05 -0300

    bookings.new ready in the car showpage

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
 
-  # validates :price, :date_from, :date_until, presence: true
-  # car need to be from the owner
-  # car must be available for booking to happen
-  # validates :date_from, comparison: { greater_than: :date_until }
+  validates :price, :date_from, :date_until, presence: true
+  # Car must be available for booking to happen - TBV
+  # Cant rent for pas date - TBV
+  # validates :date_from, comparison: { greater_than: :date.now }
+  validates :date_until, comparison: { greater_than: :date_from }
 end
