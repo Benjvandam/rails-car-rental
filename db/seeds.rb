8e57547213aba37d1958ae0df31bde7a85dc78ba
Humberto Pedra <hphumbertopedra@yahoo.com.br>
2023-08-31 18:34:39 -0300

    seeds almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,24 +11,25 @@ Car.destroy_all
 Message.destroy_all
 Chatroom.destroy_all
 User.destroy_all
+Review.destroy_all
 array_of_users = []
 
 emails = ["humberto@lewagon.com", "matt@lewagon.com", "ben@lewagon.com", "pedro@lewagon.com" ]
 
 names = ["humberto", "matt", "ben", "pedro"]
 
-user_photos = []
+user_photos = ["https://ca.slack-edge.com/T02NE0241-U05H2NBRFFY-2422604e0f19-512", "https://ca.slack-edge.com/T02NE0241-U05HJTYFZHP-e8affc977624-512","https://ca.slack-edge.com/T02NE0241-U05GUFPNAF9-29d236e53e03-512", "https://ca.slack-edge.com/T02NE0241-U05GAH9GN5D-d73433a0850a-512"]
 #Orange Lambo
 car_one = ["https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/masterpieces/gallardo-lp-550-2-2/Gallardo%20LP%20550-2-HEADER.jpg","https://cdn.dealeraccelerate.com/adrenalin/1/1706/44847/1920x1440/2004-lamborghini-gallardo-coupe-twin-turbo","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGIPpLdPOtS7PO_cexcppAOL-cbf8vY5tjqA-9xxMEuhPt1__CB1PMXrXYOfmfDWeiyyQ&usqp=CAU"]
 #BMW
 car_two = ["https://i.pinimg.com/originals/3c/99/c6/3c99c6d11bad8e68fdc1c1715adf1199.jpg","https://s.yimg.com/ny/api/res/1.2/p9A9czFvi2lQy0VCwFVN3w--/YXBwaWQ9aGlnaGxhbmRlcjtoPTY2Ng--/https://s.yimg.com/os/en_US/News/BusinessInsider/as-a-plug-in-hybrid-sports-car-bmw-i8-is-unlike-anything-on-the-road-today.jpg","https://www.autohausbr.com/wp-content/uploads/2022/04/FF9098BD-69A6-49F9-B6CB-740682D1E292-933x1400.jpeg?&"]
 #Rolls Royce phantom
-car_three = ["https://www.planetcarsz.com/assets/uploads/2021/04/ROLLS%20ROYCE%20WRAITH%20BLACK%20BADGE%20OVERDODE%20BY%20SPOFEC%202021%2002.jpg","https://www.carscoops.com/wp-content/uploads/2019/03/f2cf2d0a-rolls-royce-phantom-tranquillity-4.jpg","https://www.motortrend.com/uploads/2022/11/002-2022-Rolls-Royce-Ghost-Black-Badge.jpg?fit=around%7C875:492"]
+car_three = ["https://static.wikia.nocookie.net/pixar/images/6/63/Disney-cars-mcqueen.jpg/revision/latest?cb=20110727053733","https://i0.wp.com/pixarpost.com/wp-content/uploads/2020/10/cb295-lightning-mcqueen-cars-3-design-pixar-post-01.png?fit=1200%2C712&ssl=1","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQDSXIGzc5EkXs9FVW5J6nkeC4AmZktzInnA&usqp=CAU"]
 
 car_photos = [car_one, car_two, car_three]
 
 review_description = ["Very nice car", "Fast and confortable", "GOAT"]
-booking_price = [rand.]
+
 
 puts "Starting to seed..."
 
@@ -45,10 +46,14 @@ end
 
 puts "Seeding cars..."
 3.times do |i|
+
+  puts " creating car n. #{i + 1}"
+
   manufacturer = Faker::Vehicle.make
   car = Car.new(brand: manufacturer, model: Faker::Vehicle.model(make_of_model: manufacturer),
                 year: (1960..2023).to_a.sample, description: Faker::Quote.famous_last_words,
-                location: Faker::University.name, user: array_of_users.sample, title: Faker::Cannabis.strain)
+                location: Faker::University.name, user: array_of_users.sample, title: Faker::Cannabis.strain,
+                price: rand(50.0..100_000.0))
   file1 = URI.open(car_photos[i][0])
   file2 = URI.open(car_photos[i][1])
   file3 = URI.open(car_photos[i][2])
@@ -62,7 +67,7 @@ end
 
 puts "Seeding bookings..."
 3.times do |i|
-  booking = Booking.new(car_id: Car.all.sample.id, price: review_description[i], rating: rand(1..5), user_id: User.all.sample.id)
+  booking = Booking.new(car_id: Car.all.sample.id, date_from: Date.new(2023, 8, 31), date_until: Date.new(2023, 9, 15), user_id: User.all.sample.id)
 end
 
 
